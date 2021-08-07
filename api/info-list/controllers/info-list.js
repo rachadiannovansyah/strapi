766b6d8e83a5298620d9f0a276fa09fe390f7519
Novansyah <rachadian.novansyah15@gmail.com>
2021-08-07 07:41:27 +0700

    strapi custom

---
+++ api/info-list/controllers/info-list.js
@@ -0,0 +1,72 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.restaurant.search(ctx.query);
+    } else {
+      entities = await strapi.services.restaurant.find(ctx.query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.restaurant }));
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.restaurant.findOne({ id });
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  count(ctx) {
+    if (ctx.query._q) {
+      return strapi.services.restaurant.countSearch(ctx.query);
+    }
+    return strapi.services.restaurant.count(ctx.query);
+  },
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.create(data, { files });
+    } else {
+      entity = await strapi.services.restaurant.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.restaurant.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.restaurant.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  async healthCheck(ctx) {
+    return "health ok"
+  }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0a42ee60-8b6d-434f-a6be-58f3afbc7811'
+};
